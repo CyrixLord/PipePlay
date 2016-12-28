f6d806f5c021813c0756f9ed29cc52bf9e8e90c7
AaronEd <aaron.edmonds@outlook.com>
2016-12-28 12:53:06 -0800

    A and B executables to use as A | B pipe excersise

---
+++ A/PIPEPLAYSOLUTION/A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A: I am producing this output");
+            Console.WriteLine("Press a key to end...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ A/PIPEPLAYSOLUTION/B/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String crap;
+            Console.WriteLine("I am waiting for input!!!1");
+            crap = Console.ReadLine();
+            Console.WriteLine("B: I just intercepted[" + crap + "]!");
+            Console.WriteLine("Press <ENTER> to quit...");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
